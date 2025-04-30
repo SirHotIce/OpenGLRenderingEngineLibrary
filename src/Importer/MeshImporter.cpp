36d20acc882b7be9bd8ac08f391d7aa18ed7f306
SirHotIce <86868592+SirHotIce@users.noreply.github.com>
2025-04-29 22:57:54 -0400

    FIXED something..

--- src/Importer/MeshImporter.cpp
+++ src/Importer/MeshImporter.cpp
@@ -14,23 +14,24 @@ namespace Importer {
         if (!scene) {
             DebugLog::EngineLog::printWarning("Failed to load mesh");
             DebugLog::EngineLog::printWarning(importer.GetErrorString());
-        }
-        for (int i= 0; i<scene->mMeshes[0]->mNumVertices; i++) {
-            aiVector3D loadedVertex = scene->mMeshes[0]->mVertices[i];
-            MeshHandling::Vertex vertex;
-            vertex.Position = glm::vec3(loadedVertex.x, loadedVertex.y, loadedVertex.z);
-            aiVector3D loadedNormal = scene->mMeshes[0]->mNormals[i];
-            vertex.Normal = glm::vec3(loadedNormal.x, loadedNormal.y, loadedNormal.z);
-            aiVector3D loadedUVS= scene->mMeshes[0]->mTextureCoords[0][i];
-            vertex.TexCoords = glm::vec2(loadedUVS.x, loadedUVS.y);
+        }else {
+            for (int i= 0; i<scene->mMeshes[0]->mNumVertices; i++) {
+                aiVector3D loadedVertex = scene->mMeshes[0]->mVertices[i];
+                MeshHandling::Vertex vertex;
+                vertex.Position = glm::vec3(loadedVertex.x, loadedVertex.y, loadedVertex.z);
+                aiVector3D loadedNormal = scene->mMeshes[0]->mNormals[i];
+                vertex.Normal = glm::vec3(loadedNormal.x, loadedNormal.y, loadedNormal.z);
+                aiVector3D loadedUVS= scene->mMeshes[0]->mTextureCoords[0][i];
+                vertex.TexCoords = glm::vec2(loadedUVS.x, loadedUVS.y);
 
-            mesh.vertices.push_back(vertex);
-        }
-        if (scene->mMeshes[0]->mNumFaces>0) {
-            for (int i= 0; i<scene->mMeshes[0]->mNumFaces; i++) {
-                aiFace face = scene->mMeshes[0]->mFaces[i];
-                for (int j= 0; j<face.mNumIndices; j++) {
-                    mesh.indices.push_back(face.mIndices[j]);
+                mesh.vertices.push_back(vertex);
+            }
+            if (scene->mMeshes[0]->mNumFaces>0) {
+                for (int i= 0; i<scene->mMeshes[0]->mNumFaces; i++) {
+                    aiFace face = scene->mMeshes[0]->mFaces[i];
+                    for (int j= 0; j<face.mNumIndices; j++) {
+                        mesh.indices.push_back(face.mIndices[j]);
+                    }
                 }
             }
         }
--- src/Importer/TextureImporter.cpp
+++ src/Importer/TextureImporter.cpp
@@ -11,19 +11,20 @@ namespace Importer {
         unsigned char* texData= stbi_load(path.c_str(),&tex.width, &tex.height, &tex.bitDepth, STBI_rgb_alpha);
         if (!texData) {
             DebugLog::EngineLog::printWarning("Failed to load texture");
+        }else {
+            glGenTextures(1, &tex.texture);
+            glBindTexture(GL_TEXTURE_2D, tex.texture);
+
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, tex.width, tex.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, texData);
+            glGenerateMipmap(GL_TEXTURE_2D);
+            glBindTexture(GL_TEXTURE_2D, 0);
+            stbi_image_free(texData);
         }
-        glGenTextures(1, &tex.texture);
-        glBindTexture(GL_TEXTURE_2D, tex.texture);
-
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, tex.width, tex.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, texData);
-        glGenerateMipmap(GL_TEXTURE_2D);
-        glBindTexture(GL_TEXTURE_2D, 0);
-        stbi_image_free(texData);
 
         return tex;
     }
